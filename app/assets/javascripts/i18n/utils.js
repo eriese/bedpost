dbfefc5d8d07577404bf833927d55c479628c812
Enoch Riese <enoch.riese@gmail.com>
2019-01-29 00:36:09 -0600

    more types of pluralization with fallbacks

---
+++ app/assets/javascripts/i18n/utils.js
@@ -0,0 +1,16 @@
+I18n.pluralization["en"] = function(count) {
+	switch(parseInt(count)) {
+		case 0: return ["zero", "other"];
+		case 1: return ["one", "other"];
+		case 2: return ["two", "other"];
+		case 3: return ["three", "other"];
+		case 4: return ["four", "other"];
+		case 5: return ["five", "other"];
+		case 6: return ["six", "other"];
+		case 7: return ["seven", "other"];
+		case 8: return ["eight", "other"];
+		case 9: return ["nine", "other"];
+		case 10: return ["ten", "other"];
+		default: return ["other"]
+	}
+}
---
+++ config/locales/plurals.rb
@@ -0,0 +1,27 @@
+require "i18n/backend/pluralization"
+
+I18n::Backend::Simple.send(:include, I18n::Backend::Pluralization)
+
+FIRST_ELEVEN = {
+	0 => :zero,
+	1 => :one,
+	2 => :two,
+	3 => :three,
+	4 => :four,
+	5 => :five,
+	6 => :six,
+	7 => :seven,
+	8 => :eight,
+	9 => :nine,
+	10 => :ten
+}
+
+I18n.backend.store_translations :en, i18n: {
+	plural: {
+		default_key: :other,
+		required_keys: [:zero, :one, :other],
+		rule: lambda { |n|
+			FIRST_ELEVEN.has_key?(n) ? FIRST_ELEVEN[n] : :other
+		}
+	}
+}
