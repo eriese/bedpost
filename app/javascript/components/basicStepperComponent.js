286986442ee8e566b81e9772ea0ebf3768841146
Enoch Riese <enoch.riese@gmail.com>
2019-02-11 15:33:23 -0600

    style who page

--- app/javascript/components/basicStepperComponent.js
+++ app/javascript/components/basicStepperComponent.js
@@ -1,14 +1,48 @@
 import {animOut, animIn, processClickData} from "../modules/transitions";
 
+
+/**
+ * A component that allows you to put steps on a page and animate navigation
+ * @module
+ * @example
+ * <caption>It's easiest to put the stepper in your html with an inline template. Then use v-ifs to separate your steps,
+ * and call {@link module:components/basicStepperComponent.setStep} to change steps</caption>
+ * <basic-stepper inline-template :default-props="{animLength: 0.2}">
+ * 	<div v-if="step == 0">
+ * 		I'm the first step
+ * 		<button type="button" @click="setStep(1, {animationType: 'slideLeft'})">Go to the second step</button><
+ * 	</div>
+ * 	<div v-if="step == 1">
+ * 		I'm the second step
+ * 		<button type="button" @click="setStep(0, {animationType: 'slideRight'})">Go back to the first step</button>
+ * 	</div>
+ * </basic-stepper>
+ * @example
+ * <caption>You can also put it on the page using a scoped slot.</caption>
+ * <basic-stepper>
+ * 	<div slot-scope={step, setStep}>
+ * 		... see above example
+ * 	</div>
+ * </basic-stepper>
+ */
 export default {
 	name: "basic_stepper",
+	/**
+	 * @name module:components/basicStepperComponent~data
+	 * @type {Object}
+	 * @property {number} step=0 the step the stepper is currently showing
+	 */
 	data: function () {
 		return {
 			step: 0
 		}
 	},
+	/**
+	 * @type {Object}
+	 * @property {Boolean} [animate=true] whether step transitions should animate
+	 * @property {module:transitions.prop_overrides} [defaultProps] default animation props to apply to animations
+	 */
 	props: {
-		stepClass: String,
 		animate: {
 			type: Boolean,
 			default: true
@@ -17,8 +51,19 @@ export default {
 			type: Object
 		}
 	},
+	/** @lends module:components/basicStepperComponent */
 	methods: {
+		/**
+		 * set what step the stepper is on (click handler for navigation)
+		 * @function
+		 * @param {number} newStep   the index of the step to go to
+		 * @param {module:transitions.prop_overrides} [animProps= module:components/basicStepperComponent.props.defaultProps] properties to use in the transition animation
+		 */
 		setStep(newStep, animProps) {
+			if (!this.animate) {
+				this.step = newStep;
+				return;
+			}
 			let props = Object.assign({}, this.defaultProps, animProps)
 			processClickData(props)
 			animOut(() => {
@@ -26,5 +71,12 @@ export default {
 				this.$nextTick(animIn)
 			});
 		}
+	},
+	// render without template
+	render: function() {
+		return this.$scopedSlots.default({
+			step: this.step,
+			setStep: this.setStep
+		})
 	}
 }
