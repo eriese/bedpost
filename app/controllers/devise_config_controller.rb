266b38009ff336e663908b99a146b73c6ea5e1d3
Enoch Riese <enoch.riese@gmail.com>
2019-10-14 14:56:40 -0500

    add devise flash messaging

--- app/controllers/devise_config_controller.rb
+++ app/controllers/devise_config_controller.rb
@@ -3,6 +3,10 @@ class DeviseConfigController < ApplicationController
 	self.responder = DeviseResponder
 	respond_to :json, :html
 
+	def is_flashing_format?
+		true
+	end
+
 	def render *args
 		gon_client_validators(resource) unless @gon == true
 		super
