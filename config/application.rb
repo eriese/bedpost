d387e9f67b72ac0300c90170a7565a2be14b9c98
Enoch Riese <enoch.riese@gmail.com>
2019-10-23 10:58:09 -0500

    conservative update all gems

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,7 @@ module Bedpost
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :en
+    config.i18n.fallbacks = [I18n.default_locale]
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
     config.i18n.available_locales = %w(en)
     config.middleware.use I18n::JS::Middleware
