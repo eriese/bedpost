cc0a28de340503636cc8ad4494b0fae009348468
Enoch Riese <enoch.riese@gmail.com>
2019-10-24 18:11:53 -0500

    fix up classes implementing static resource

--- spec/models/contact/instrument_spec.rb
+++ spec/models/contact/instrument_spec.rb
@@ -46,83 +46,38 @@ RSpec.describe Contact::Instrument, type: :model do
   			end
   		end
   	end
-
-  	pending '#show_for_user' do
-  		it 'returns false if a user is not capable of a kind of contact' do
-  			inst = build(:contact_instrument, conditions: {can_penetrate: [:can_penetrate?]})
-  			user = double(can_penetrate?: false)
-
-  			result = inst.show_for_user(user, Contact::ContactType::PENETRATED)
-  			expect(result).to be false
-  		end
-
-  		it 'returns false if a user does not have the instrument' do
-  			inst = build(:contact_instrument, conditions: {all: [:can_penetrate?]})
-  			user = double(can_penetrate?: false)
-
-  			result = inst.show_for_user(user, Contact::ContactType::PENETRATED)
-  			expect(result).to be false
-  		end
-
-  		it 'gets the conditions for the normal version if there are no special conditions for _self' do
-  			inst = build(:contact_instrument, conditions: {can_penetrate: [:can_penetrate?]})
-  			user = double(can_penetrate?: false)
-
-  			result = inst.show_for_user(user, Contact::ContactType::PENETRATED_self)
-  			expect(result).to be false
-  		end
-
-  		it 'can check multiple conditions in any order' do
-  			inst = build(:contact_instrument, conditions: {can_penetrate: [:can_penetrate?, :has_internal?]})
-  			user = double(can_penetrate?: true, has_internal?: false)
-  			user2 = double(can_penetrate?: false, has_internal?: true)
-  			user3 = double(can_penetrate?: true, has_internal?: true)
-
-  			result1 = inst.show_for_user(user, Contact::ContactType::PENETRATED)
-  			expect(result1).to be false
-  			result2 = inst.show_for_user(user2, Contact::ContactType::PENETRATED)
-  			expect(result2).to be false
-  			result3 = inst.show_for_user(user3, Contact::ContactType::PENETRATED)
-  			expect(result3).to be true
-  		end
-
-  		it 'gracefully handles empty conditions' do
-  			inst = build(:contact_instrument)
-  			expect(inst.show_for_user(double(), Contact::ContactType::PENETRATED)).to be true
-
-  			inst2 = build(:contact_instrument, conditions: {})
-  			expect(inst2.show_for_user(double(), Contact::ContactType::PENETRATED)).to be true
-  		end
-  	end
   end
 
   context 'class methods' do
-    describe '.by_name' do
-      before :all do
-        @names = ["hand", "mouth", "toys"]
-        @names.each {|n| create(:contact_instrument, name: n)}
+    describe '#hashed_for_partnership' do
+      it 'caches based on user and partner id and update times' do
+        user = build_stubbed(:user_profile)
+        partner = build_stubbed(:profile)
+        allow(Contact::Instrument).to receive(:as_map).and_call_original
+        Contact::Instrument.hashed_for_partnership(user, partner)
+        expect(Contact::Instrument).to have_received(:as_map).once
+
+        Contact::Instrument.hashed_for_partnership(user, partner)
+        expect(Contact::Instrument).to have_received(:as_map).once
+
+        user.updated_at += 1.day
+        Contact::Instrument.hashed_for_partnership(user, partner)
+        expect(Contact::Instrument).to have_received(:as_map).twice
       end
 
-      after :all do
-        Contact::Instrument.delete_all
-      end
+      it 'adds the overrides for the partner and user to the serialized hash' do
+        user = build_stubbed(:user_profile)
+        partner = build_stubbed(:profile)
+        inst = build_stubbed(:contact_instrument, name: :external_genitals, user_override: :external_name)
 
-      it 'gets a hash of instruments with names as keys' do
-        result = Contact::Instrument.by_name
-        expect(result).to be_a(Hash)
-        @names.each {|n| expect(result).to have_key(n)}
-      end
+        allow(Contact::Instrument).to receive(:as_map) {
+          Hash[inst.id, inst]
+        }
 
-      it 'gets a hash that gives access for strings or symbols' do
-        names = Contact::Instrument.by_name
-        expect(names[:hand]).to eq names["hand"]
-      end
-
-      it 'caches' do
-        names = Contact::Instrument.by_name
-        allow(Contact::Instrument).to receive(:all){[]}
-        Contact::Instrument.by_name
-        expect(Contact::Instrument).to_not have_received(:all)
+        result = Contact::Instrument.hashed_for_partnership(user, partner)
+        expect(result[inst.id]).to be_a(Hash)
+        expect(result[inst.id][:user_name]).to eq user.external_name
+        expect(result[inst.id][:partner_name]).to eq partner.external_name
       end
     end
   end
