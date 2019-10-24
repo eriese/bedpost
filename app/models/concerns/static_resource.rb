db4f786dd1ffec4abd817184aaff9e71e3b4c16d
Enoch Riese <enoch.riese@gmail.com>
2019-10-24 17:39:13 -0500

    set up caching in static_resource concern

--- app/models/concerns/static_resource.rb
+++ app/models/concerns/static_resource.rb
@@ -1,34 +1,34 @@
 module StaticResource
 	extend ActiveSupport::Concern
 
+	included do
+		after_save :clear_all_caches if respond_to? :after_save
+		after_destroy :clear_all_caches if respond_to? :after_destroy
+	end
+
+	def clear_all_caches
+		Rails.cache.delete_matched(//, namespace: self.class.name)
+	end
+
 	class_methods do
+		def list
+			Rails.cache.fetch("list", namespace: name) {all.to_a}
+		end
+
 		def as_map
-			unless Rails.env.development?
-				@as_map ||= hash_all
-			else
-				hash_all
-			end
+			Rails.cache.fetch("as_map", namespace: name) {HashWithIndifferentAccess[all.map {|i| [i.id, i]}]}
 		end
 
 		def grouped_by(column, instantiate=true)
-			@grouped_queries ||= {}
+			Rails.cache.fetch("#{column}_#{instantiate}", namespace: name) do
+				query = collection.aggregate([{"$group" => {"_id" => "$#{column}", "members" => {"$push"=> "$$ROOT"}}}])
 
-			query = collection.aggregate([{"$group" => {"_id" => "$#{column}", "members" => {"$push"=> "$$ROOT"}}}])
-
-			if Rails.env.development?
-				hash_query(query, instantiate)
-			else
-				@grouped_queries["#{column}_#{instantiate}"] ||= hash_query(query, instantiate)
+				HashWithIndifferentAccess[query.map do |col|
+					members = col[:members]
+					[col[:_id], instantiate ? members.map { |m| new(m) } : members ]
+				end
+				]
 			end
-		end
-
-		private
-		def hash_query(query, instantiate)
-			HashWithIndifferentAccess[query.map {|col| [col[:_id], instantiate ? col[:members].map { |m| new(m) } : col[:members] ]}]
-		end
-
-		def hash_all
-			HashWithIndifferentAccess[all.map {|i| [i.id, i]}]
 		end
 	end
 end
---
+++ spec/models/concerns/static_resource_spec.rb
@@ -0,0 +1,166 @@
+require 'rails_helper'
+
+class StaticResourceTestModel
+	include Mongoid::Document
+	include StaticResource
+
+	field :f_1
+	field :f_2
+end
+
+RSpec.describe StaticResource, type: :module do
+	def make_models(num_models, f_1: nil, f_2: nil)
+		num_models.times { |t| StaticResourceTestModel.create(f_1: f_1, f_2: f_2) }
+	end
+
+	def cache_data
+		Rails.cache.instance_variable_get(:@data)
+	end
+
+	def in_cache?(cache_name = "list")
+		Rails.cache.exist?(cache_name, namespace: StaticResourceTestModel.name)
+	end
+
+	after :each do
+		Rails.cache.clear
+		StaticResourceTestModel.destroy_all
+	end
+
+	describe '#clear_all_caches' do
+		it 'clears all caches for the class when any instance is updated' do
+			make_models(3)
+			StaticResourceTestModel.list
+			StaticResourceTestModel.as_map
+			expect(cache_data.size).to be 2
+
+			model = StaticResourceTestModel.first
+			expect(model).to receive(:clear_all_caches).and_call_original
+			model.update(f_1: "changed")
+			expect(cache_data).to be_empty
+		end
+
+		it 'clears all caches for the class when any new instance is created' do
+			make_models(3)
+			StaticResourceTestModel.list
+			expect(in_cache?).to be true
+
+			model = StaticResourceTestModel.new
+			expect(model).to receive(:clear_all_caches).and_call_original
+			model.save
+			expect(in_cache?).to be false
+		end
+
+		it 'clears all caches for the class when an instance is destroyed' do
+			make_models(3)
+			StaticResourceTestModel.list
+			expect(in_cache?).to be true
+
+			model = StaticResourceTestModel.last
+			expect(model).to receive(:clear_all_caches).and_call_original
+			model.destroy
+			expect(in_cache?).to be false
+		end
+
+		it 'only clears its own caches' do
+			make_models(3)
+			StaticResourceTestModel.list
+			Rails.cache.write("other cache", "some string")
+
+			StaticResourceTestModel.first.clear_all_caches
+			expect(in_cache?).to be false
+			expect(Rails.cache.exist?("other cache")).to be true
+		end
+
+		it 'keeps to itself even with similar class names' do
+			make_models(3)
+			StaticResourceTestModel.list
+			other_namespace = StaticResourceTestModel.name + " "
+			Rails.cache.write("other", "some string", namespace: other_namespace)
+
+			StaticResourceTestModel.first.clear_all_caches
+			expect(in_cache?).to be false
+			expect(Rails.cache.exist?("other", namespace: other_namespace)).to be true
+		end
+	end
+
+	context 'class_methods' do
+		describe '#list' do
+			it 'get an array of all entries from the class' do
+				num_models = 3
+				make_models(num_models)
+				result = StaticResourceTestModel.list
+				expect(result).to be_a(Array)
+				expect(result.size).to be num_models
+			end
+
+			it 'caches after the first hit' do
+				make_models(3)
+				expect(StaticResourceTestModel).to receive(:all).once.and_call_original
+				StaticResourceTestModel.list
+				expect(in_cache?).to be true
+				StaticResourceTestModel.list
+			end
+		end
+
+		describe 'as_map' do
+			it 'gets a HashWithIndifferentAccess of all entries with their ids as keys' do
+				num_models = 3
+				make_models(num_models)
+				result = StaticResourceTestModel.as_map
+				expect(result).to be_a(HashWithIndifferentAccess)
+				expect(result.size).to be num_models
+			end
+
+			it 'caches after the first hit' do
+				make_models(3)
+				expect(StaticResourceTestModel).to receive(:all).once.and_call_original
+				StaticResourceTestModel.as_map
+				expect(in_cache?("as_map")).to be true
+				StaticResourceTestModel.as_map
+			end
+		end
+
+		describe 'grouped_by' do
+			it 'caches by column and instantiation arguments' do
+				make_models(2, f_1: "first")
+				make_models(3, f_1: "second")
+				StaticResourceTestModel.grouped_by(:f_1)
+				expect(in_cache?("f_1_true")).to be true
+				StaticResourceTestModel.grouped_by(:f_1, false)
+				expect(in_cache?("f_1_false")).to be true
+			end
+
+			context 'with instantiate = true (default)' do
+				it 'returns a hash of arrays of all entries keyed by their values for the given column' do
+					num_first = 3
+					make_models(num_first, f_1: "first")
+					num_second = 4
+					make_models(num_second, f_1: "second")
+
+					result = StaticResourceTestModel.grouped_by(:f_1)
+					expect(result).to be_a(HashWithIndifferentAccess)
+					expect(result.keys.size).to be 2
+					expect(result[:first].size).to be num_first
+					expect(result["second"].size).to be num_second
+					expect(result[:first][0]).to be_a(StaticResourceTestModel)
+				end
+			end
+
+			context 'with instantiate = false' do
+				it 'returns a hash of arrays of all entries keyed by their values for the given column' do
+					num_first = 3
+					make_models(num_first, f_1: "first")
+					num_second = 4
+					make_models(num_second, f_1: "second")
+
+					result = StaticResourceTestModel.grouped_by(:f_1, false)
+					expect(result).to be_a(HashWithIndifferentAccess)
+					expect(result.keys.size).to be 2
+					expect(result[:first].size).to be num_first
+					expect(result["second"].size).to be num_second
+					expect(result[:first][0]).to be_a(Hash)
+				end
+			end
+		end
+	end
+end
