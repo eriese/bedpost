c3d96ee1596e179cf557bdd84c7995b2c05d5050
Enoch Riese <enoch.riese@gmail.com>
2019-11-21 13:44:30 -0600

    upping ruby version and updating gsap

--- app/javascript/components/NavComponent.js
+++ app/javascript/components/NavComponent.js
@@ -1,11 +1,12 @@
-import {TimelineMax, TweenMax} from "gsap/TweenMax";
-import renderless from "@mixins/renderless";
+import { gsap } from 'gsap';
+import renderless from '@mixins/renderless';
 
 /**
  * The nav menu component
+ *
  * @module
  * @vue-data {Boolean} isOpen=false is the navigation menu open?
- * @vue-data {TimelineMax} timeLine the animation timeline for opening and closing
+ * @vue-data {timeline} timeLine the animation timeline for opening and closing
  */
 export default {
 	data: function() {
@@ -13,9 +14,9 @@ export default {
 			isOpen: false,
 			timeLine: null,
 			menu: null
-		}
+		};
 	},
-	props: ["inner"],
+	props: ['inner'],
 	mixins: [renderless],
 	computed: {
 		slotScope: function() {
@@ -23,26 +24,27 @@ export default {
 				isOpen: this.isOpen,
 				openMenu: this.openMenu,
 				toggleMenu: this.toggleMenu
-			}
+			};
 		}
 	},
 	// on component mounted
 	mounted() {
 		this.menu = this.$scopedSlots.default()[0].context.$refs.menu;
 		// display the menu so the timeline can get correct data about its starting state
-		this.menu.style.display = "block"
+		this.menu.style.display = 'block';
 		// make a new timeline
-		this.timeLine = new TimelineMax()
+		this.timeLine = gsap.timeline();
 		// tween the width till it's closed
-		this.timeLine.to(this.menu, 0.3, {width: 0})
+		this.timeLine.to(this.menu, 0.3, {width: 0});
 		// then set display none
-		this.timeLine.set(this.menu, {display: "none", immediateRender: false});
+		this.timeLine.set(this.menu, {display: 'none', immediateRender: false});
 		// set the timeline to complete
 		this.timeLine.progress(1);
 	},
 	methods: {
 		/**
 		 * Open the menu and add a click-outside listener {@link module:components/navComponent.documentClick}
+		 *
 		 * @listens document.body.click
 		 */
 		openMenu() {
@@ -50,7 +52,7 @@ export default {
 			// play the timeline in reverse
 			this.timeLine.reverse();
 			// listen for clicks on the body for a click-out close
-			document.body.addEventListener('click', this.documentClick)
+			document.body.addEventListener('click', this.documentClick);
 		},
 		/**
 		 * Close the menu
@@ -60,19 +62,20 @@ export default {
 			// play to timeline forward
 			this.timeLine.play();
 			// stop listening for click outside
-			document.body.removeEventListener('click', this.documentClick)
+			document.body.removeEventListener('click', this.documentClick);
 		},
 		/**
 		 * React to a click on the document while the menu is open.
 		 * Close the menu if the click was on a menu link or outside the menu element
+		 *
 		 * @param  {Event} e the click event
 		 */
 		documentClick(e) {
 			let menu = this.menu;
 			let target = e.target;
 			// if the target is a link or not on the menu, close it
-			if (target.tagName == "A" || (menu !== target & !menu.contains(target))) {
-				this.closeMenu()
+			if (target.tagName == 'A' || (menu !== target & !menu.contains(target))) {
+				this.closeMenu();
 			}
 		},
 		/**
@@ -87,4 +90,4 @@ export default {
 		}
 
 	}
-}
+};
