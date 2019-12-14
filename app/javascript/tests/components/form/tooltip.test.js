c6e84cfd9c95a61ba0bc7774f9a4c0ca9a913cd0
Enoch Riese <enoch.riese@gmail.com>
2019-12-13 22:57:41 -0600

    trying to regain some test coverage

--- app/javascript/tests/components/form/tooltip.test.js
+++ app/javascript/tests/components/form/tooltip.test.js
@@ -48,6 +48,54 @@ describe('Tooltip component', () => {
 		});
 	});
 
+	describe('fieldFocused watcher', () => {
+		beforeAll(() => {
+			jest.useFakeTimers();
+		});
+		afterAll(() => {
+			jest.useRealTimers();
+		});
+
+		it('sets a timeout to delay showing or hiding the tooltip when fieldFocused == true', () => {
+			const wrapper = mount(Tooltip, {
+				propsData: {
+					useScope: {focused: false, hovered: false}
+				}
+			});
+
+			wrapper.setProps({
+				useScope: {focused: true, hovered: false}
+			});
+
+			expect(wrapper.vm.tippyProps.visible).toBe(false);
+			jest.runAllTimers();
+
+			expect(wrapper.vm.tippyProps.visible).toBe(true);
+		});
+
+		it('cancels the running timer if the value changes before the tooltip showed/hid', () => {
+			const wrapper = mount(Tooltip, {
+				propsData: {
+					useScope: {focused: false, hovered: false}
+				}
+			});
+
+			wrapper.setProps({
+				useScope: {focused: true, hovered: false}
+			});
+
+			jest.advanceTimersByTime(50);
+
+			wrapper.setProps({
+				useScope: {focused: false, hovered: false}
+			});
+
+			jest.runAllTimers();
+
+			expect(wrapper.vm.tippyProps.visible).toBe(false);
+		});
+	});
+
 	describe('with scope', () => {
 		it('sets a manual trigger', () => {
 			const scope = {
