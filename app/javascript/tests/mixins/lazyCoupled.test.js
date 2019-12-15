f57809515b929cf10cbe203d93801da08bc49e02
Enoch Riese <enoch.riese@gmail.com>
2019-12-15 12:55:58 -0600

    lazy-coupled chaining

--- app/javascript/tests/mixins/lazyCoupled.test.js
+++ app/javascript/tests/mixins/lazyCoupled.test.js
@@ -1,29 +1,29 @@
-import {shallowMount, mount} from "@vue/test-utils";
-import {lazyChild, lazyParent} from "@mixins/lazyCoupled";
+import {mount} from '@vue/test-utils';
+import {lazyChild, lazyParent} from '@mixins/lazyCoupled';
 
-describe("Lazy coupled mixins", () => {
+describe('Lazy coupled mixins', () => {
 	const child = {
-		template: "<div/>",
+		template: '<div class="child-component"/>',
 		mixins: [lazyChild]
-	}
+	};
 
-	describe("With both parent and child mounted", () => {
+	describe('With both parent and child mounted', () => {
 		const parent = {
-			template: "<div>{{msg}}<child/></div>",
+			template: '<div>{{msg}}<child/></div>',
 			mixins: [lazyParent],
 			data: function() {
 				return {
 					msg: false
-				}
+				};
 			},
 			methods: {
 				onChildMounted: function() {
-					this.msg = "emitted";
+					this.msg = 'emitted';
 				}
 			}
-		}
+		};
 
-		test("The child emits lazy-child-present on the parent", () => {
+		test('The child emits lazy-child-present on the parent', () => {
 			let pWrapper = mount(parent, {
 				stubs: {
 					child
@@ -31,9 +31,9 @@ describe("Lazy coupled mixins", () => {
 			});
 
 			expect(pWrapper.emitted()['lazy-child-present']).toHaveLength(1);
-		})
+		});
 
-		test("The onChildMounted function of the parent is called", async () => {
+		test('The onChildMounted function of the parent is called', async () => {
 			const spy = jest.spyOn(parent.methods, 'onChildMounted');
 
 			const pWrapper = mount(parent, {
@@ -43,9 +43,56 @@ describe("Lazy coupled mixins", () => {
 			});
 
 			await pWrapper.vm.$nextTick();
-			expect(pWrapper.vm.msg).toBe("emitted")
+			expect(pWrapper.vm.msg).toBe('emitted');
 
 			expect(spy).toHaveBeenCalled();
-		})
-	})
-})
+		});
+	});
+
+	describe('a component that is a lazy parent and a lazy child', () => {
+		afterEach(() => {
+			jest.useRealTimers();
+		});
+
+		it('does not emit lazy-child-present until its child has emitted', async () => {
+			const child = {
+				template: '<div/>',
+				mounted() {
+					setTimeout(() => this.$parent.$emit('lazy-child-present'), 100);
+				}
+			};
+
+			const middle = {
+				template: '<div><child/></div>',
+				mixins: [lazyParent, lazyChild],
+				methods: {
+					onChildMounted: jest.fn()
+				},
+				components: {
+					'child': child
+				}
+			};
+
+			const parent = {
+				template: '<div><middle/></div>',
+				mixins: [lazyParent],
+				methods: {
+					onChildMounted: jest.fn()
+				},
+				components: {
+					'middle': middle
+				}
+			};
+
+			jest.useFakeTimers();
+			const wrapper = mount(parent);
+
+			await wrapper.vm.$nextTick();
+			expect(wrapper.emitted('lazy-child-present')).toBeFalsy();
+
+			jest.runAllTimers();
+			await wrapper.vm.$nextTick();
+			expect(wrapper.emitted('lazy-child-present')).toBeTruthy();
+		});
+	});
+});
