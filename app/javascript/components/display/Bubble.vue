c7f0d6cad9a684035ce0b0dbc6b017cbcb0a5e2e
Enoch Riese <enoch.riese@gmail.com>
2019-12-23 16:03:53 -0600

    replace bubble with vue-tippy

--- app/javascript/components/display/Bubble.vue
+++ app/javascript/components/display/Bubble.vue
@@ -1,116 +1,71 @@
 <template>
-	<div class="bubble-container">
-		<div v-show="targetEl" class="bubble" :class="[addClass, cls]" :style="stl" ref="bubble">
+	<div class="bubble" :style="{height: heightVal}">
+		<tippy v-bind="tippyProps" ref="tippy">
 			<slot></slot>
-			<div class="arrow"></div>
-		</div>
+		</tippy>
 	</div>
 </template>
 
 <script>
-import resizeable from '@mixins/resizeable';
+import { TippyComponent } from 'vue-tippy';
+import ArrowModifier from '@plugins/popperModifiers/arrow';
 
 /**
- * A component that shows an explanation bubble with a pointer to the given target element. Automatically positions itself based on the given position property
+ * A component that shows an explanation bubble with a pointer to the given target element. Uses vue-tippy
  *
  * @module
- * @vue-data {String} cls="" classes applied to the bubble element for positioning
- * @vue-data {Object} stl={} style applied to the bubble element for positioning
- * @vue-data {Timeout} debounce the id of the debounce timeout
- * @vue-data {HTMLElement} targetEl the element the bubble is pointing to
- * @vue-prop {String} target the css selector of the element the bubble should point to
- * @vue-prop {String} position the position on the target element that the bubble should point to. (e.g. 'right' will make it point to the right edge of the target element)
- * @vue-prop {String} addClass any classes to add to the bubble element
+ * @vue-data {number} heightVal the height the element needs to make space for the tippy
+ * @vue-computed {object} tippyProps options to pass to the tippy component
  */
 export default {
-	mixins: [resizeable],
-	data: function() {
+	components: {
+		tippy: TippyComponent,
+	},
+	data() {
 		return {
-			cls: '',
-			stl: {},
-			targetEl: undefined
+			heightVal: undefined
 		};
 	},
 	props: {
 		target: String,
-		position: String,
-		addClass: String
+		position: {
+			type: String,
+			default: 'bottom'
+		},
+		theme: {
+			type: String,
+			default: 'light'
+		},
+	},
+	computed: {
+		tippyProps() {
+			return {
+				appendTo: () => this.$el,
+				arrow: true,
+				boundary: 'viewport',
+				flip: false,
+				multiple: true,
+				theme: this.theme,
+				trigger: 'manual',
+				hideOnClick: false,
+				visible: true,
+				toSelector: `#${this.target}`,
+				placement: this.position,
+				onMount: this.setHeight,
+				popperOptions: {
+					modifiers: {
+						arrow: {
+							fn: ArrowModifier
+						}
+					}
+				}
+			};
+		},
 	},
 	methods: {
-		/**
-		 * Set the position of the bubble
-		 */
-		setPosition() {
-			// get bounding rects for the target and the bubble container element
-			let targetRect = Utils.getBoundingDocumentRect(this.targetEl, true);
-			let elRect = Utils.getBoundingDocumentRect(this.$el);
-
-			// initialize an empty position object
-			let pos = {};
-			// set position starting point based on the position prop
-			switch (this.position) {
-			case 'centerX':
-				// point to target centerX
-				pos.left = targetRect.centerX;
-				break;
-			case 'right':
-				// point to target right
-				pos.left = targetRect.right;
-				break;
-			// TODO implement more positions as needed
-			}
-
-			let bubWidth = this.$refs.bubble.offsetWidth;
-			// let bubHeight = this.$refs.bubble.offsetHeight;
-
-			if (pos.left !== undefined)  {
-				// compensate for element offset
-				pos.left -= elRect.left;
-
-				// if the bubble will not fit in the element based on current left
-				if (pos.left + bubWidth > elRect.width) {
-					// if the bubble will also not fit on the other side of the current left
-					if (pos.left - bubWidth < 0) {
-						// center the bubble around the current left
-						pos.left -= bubWidth / 2;
-						this.cls = 'center-justify';
-					} else {
-						// otherwise put it all the way to right of the current left
-						pos.left -= bubWidth;
-						this.cls = 'right-justify';
-					}
-				} else {
-					// if it will fit, left-justify
-					this.cls = 'left-justify';
-				}
-
-
-				// set it to pixels
-				pos.left += 'px';
-			}
-
-			// if (pos.top !== undefined) {
-			// 	pos.top += "px";
-			// }
-
-			// set it as the style
-			this.stl = pos;
-		},
-		/**
-		 * get the target element and set the position if it is found
-		 */
-		onSize() {
-			// only get it if it hasn't already been found
-			this.targetEl = this.targetEl || document.getElementById(this.target);
-
-			// if it's there, set the position
-			if (this.targetEl) {
-				this.setPosition();
-			} else {
-				// otherwise try again after the debounce window
-				this.debounceOnSize();
-			}
+		setHeight() {
+			this.heightVal = `${this.$refs.tippy.tip.popper.offsetHeight}px`;
 		}
-	},
+	}
 };
 </script>
