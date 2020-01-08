37272ce167f75659ae6ccf58b2a975b1535c38ab
Enoch Riese <enoch.riese@gmail.com>
2020-01-08 15:33:21 -0600

    log scale the risk bar

--- app/javascript/components/display/ProgBar.vue
+++ app/javascript/components/display/ProgBar.vue
@@ -6,27 +6,49 @@
 </template>
 
 <script>
-	export default {
-		props: {
-			benchmark: {
-				type: Number,
-				default: -1
-			},
-			progress: Number,
-			max: Number,
-			id: {
-				type: String,
-				default: "progbar"
-			}
+export default {
+	props: {
+		benchmark: {
+			type: Number,
+			default: -1
 		},
-		computed: {
-			benchmarkStyle: function() {
-				if (this.benchmark < 0) {return {display: "none"}}
-				return {left: `${this.benchmark * 100 / this.max}%`}
-			},
-			progStyle: function() {
-				return {width: `${this.progress * 100 / this.max}%`}
+		progress: Number,
+		max: Number,
+		min: {
+			type: Number,
+			default: 0
+		},
+		middle: Number,
+		id: {
+			type: String,
+			default: 'progbar'
+		},
+		logScale: Boolean,
+	},
+	computed: {
+		benchmarkStyle: function() {
+			if (this.benchmark < 0) {return {display: 'none'};}
+			return {left: `${this.getPosition(this.benchmark)}%`};
+		},
+		progStyle: function() {
+			return {width: `${this.getPosition(this.progress)}%`};
+		},
+	},
+	methods: {
+		getPosition: function(value) {
+			if (value == 0) {
+				return 2;
+			}
+
+			if (this.logScale) {
+				let middle = this.middle || this.max/2;
+				const b = Math.pow(2, 1/(this.max - middle));
+				const a = 100/Math.pow(b, this.max);
+				return Math.floor(a * Math.pow(b, value));
+			} else {
+				return value * 100 / this.max;
 			}
 		}
 	}
+};
 </script>
