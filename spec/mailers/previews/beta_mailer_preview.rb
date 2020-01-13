691d8d5e5da095b9a26a83b08a7cd9d1f5bf0f3d
Enoch Riese <enoch.riese@gmail.com>
2020-01-12 22:30:20 -0600

    rename trello_mailer preview. delete empty spec

--- spec/mailers/previews/beta_mailer_preview.rb
+++ spec/mailers/previews/beta_mailer_preview.rb
@@ -1,5 +1,5 @@
 # Preview all emails at http://localhost:3000/rails/mailers/trello_mailer
-class TrelloMailerPreview < ActionMailer::Preview
+class BetaMailerPreview < ActionMailer::Preview
 	def bug_report
 		report = {
 			title: "A bug has appeared!",
@@ -10,6 +10,6 @@ class TrelloMailerPreview < ActionMailer::Preview
 			notes: ""
 		}
 
-		TrelloMailer.bug_report(report)
+		BetaMailer.bug_report(report)
 	end
 end
