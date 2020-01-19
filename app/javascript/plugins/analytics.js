ffc323715c99d8311f62c42aa1c809d1780d57a1
Enoch Riese <enoch.riese@gmail.com>
2020-01-19 15:39:03 -0600

    timing tracking on turbolinks events

--- app/javascript/plugins/analytics.js
+++ app/javascript/plugins/analytics.js
@@ -15,7 +15,79 @@ export const sendAnalyticsEvent = function (actionName, options) {
 	// if there's no analytics, do nothing
 	if (!hasGTag()) {return;}
 
+	console.log(actionName, options);
 	window.gtag('event', actionName, options);
+};
+
+/**
+ * Get the number of milliseconds since the given startTime
+ *
+ * @param  {number} [startTime=window.timing] the time the event began
+ * @return {number}           								the number of milliseconds since the startTime
+ */
+const timeSince = (startTime) => Date.now() - (startTime || window.timing);
+
+/**
+ * Send a timing event to google analytics
+ *
+ * @param  {string} name        the name of the event (not the event action name)
+ * @param  {string} category    the event category
+ * @param  {string} label       the timing event label
+ * @param  {number} eventTiming the amount of time the event took if it is different from the time since window.timing
+ */
+const sendAnalyticsTimingEvent = function (name, category, label, eventTiming) {
+	eventTiming = eventTiming !== undefined ? eventTiming : timeSince();
+
+	sendAnalyticsEvent('timing_complete', {
+		'name': name || 'load',
+		'event_category': category,
+		'event_label': label,
+		'value': eventTiming,
+	});
+};
+
+const turbolinksEventCategory = 'Tubolinks Timing';
+const turbolinksPrefix = 'turbolinks:';
+
+/**
+ * Add turbolinks listeners to track the timing between a start event and its corresponding completion event
+ * @param {string} startEvent the name of the start event without the prefix
+ * @param {string} endEvent   the name of the end event without the prefix
+ * @param {string} [eventName=endEvent]  the name of the analytics timing event to send
+ */
+function addTurbolinksListeners(startEvent, endEvent, eventName) {
+	let eventTiming;
+	document.addEventListener(`${turbolinksPrefix}${startEvent}`, () => eventTiming = Date.now());
+
+	document.addEventListener(`${turbolinksPrefix}${endEvent}`, () => {
+		sendAnalyticsTimingEvent(eventName || endEvent, turbolinksEventCategory, undefined, timeSince(eventTiming));
+	});
+}
+
+/**
+ * add timing tracking to turbolinks events
+ */
+export const addTurbolinksTracking = function() {
+
+	// add an event listener to reset the timing clock whenever a new page visit begins
+	document.addEventListener(`${turbolinksPrefix}visit`,
+		() => window.timing = Date.now());
+
+	// add an event listener to track the load timing
+	document.addEventListener(`${turbolinksPrefix}load`,
+		() => sendAnalyticsTimingEvent('load', turbolinksEventCategory));
+
+	// add event listeners to track time between start and finish of request
+	addTurbolinksListeners('request-start', 'request-end');
+	// add event listeners to track time between start and finish of render
+	addTurbolinksListeners('before-render', 'render');
+
+	// add error reporting
+	window.onerror = function(err) {
+		sendAnalyticsEvent('exception', {
+			description: err,
+		});
+	};
 };
 
 /**
@@ -27,7 +99,8 @@ export default {
 	install(Vue) {
 		Vue.mixin({
 			methods: {
-				sendAnalyticsEvent
+				sendAnalyticsEvent,
+				sendAnalyticsTimingEvent,
 			}
 		});
 	}
