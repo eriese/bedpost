7badd8ee7299f795993e0a934fc42842f85a4b04
Enoch Riese <enoch.riese@gmail.com>
2020-01-26 15:52:24 -0600

    tests to ensure safety of static controller

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,8 @@
 class StaticController < ApplicationController
+	skip_before_action :store_user_location!
+	skip_before_action :authenticate_user_profile!
+	skip_before_action :check_first_time
+
 	def static_or_404
 		view_name = params.require(:static).split('/')[-1].delete('.')
 		if lookup_context.exists?("#{view_name}.html.erb", ['static'])
