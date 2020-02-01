294b99b26eb0a096f8b0dfca960f3f1e79cd3b16
Enoch Riese <enoch.riese@gmail.com>
2020-01-31 21:12:45 -0600

    fix question finding on beta mailer

--- app/jobs/send_beta_invite_job.rb
+++ app/jobs/send_beta_invite_job.rb
@@ -1,16 +1,17 @@
 class SendBetaInviteJob < ApplicationJob
 	queue_as :default
 
-	NAME_INDEX = 2
-	EMAIL_INDEX = 1
+	NAME_INDEX = 1
+	EMAIL_INDEX = 0
 
 	def perform(survey_id, response_id)
 		url = "https://api.surveymonkey.net/v3/surveys/#{survey_id}/responses/#{response_id}/details"
 		bearer_token = Rails.application.credentials.dig(:survey_monkey, :access_token)
 		response = HTTP.auth("Bearer #{bearer_token}").get(url)
 		response_json = response.parse.with_indifferent_access
-		questions = response_json[:pages][0][:questions]
+		questions = response_json[:pages][1][:questions]
 		email = questions[EMAIL_INDEX][:answers][0][:text]
+		binding.pry
 		name = questions[NAME_INDEX][:answers][0][:text]
 		BetaMailer.beta_invite(email, name).deliver_now
 	end
